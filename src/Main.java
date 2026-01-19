506796c3e32e0c05261bb4bfaea1ec4d0991388c
Xaylhem369 <ciubucciu.larissa@edu.iisleviponti.it>
2026-01-19 11:43:52 +0100

    Commit Aha

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Aha");
-        System.out.println("Did you know that Aha used a worm to make the Emanator of Aha");
+        System.out.println("Did you know that Aha used a worm to make the Emanator of Aha aka.Elation");
     }
 }
